d8fa6475795fc57eec27f58180503d23203fa03a
Daniel Kos <unodgs@gmail.com>
2020-05-02 21:38:15 +0200

    Project bootstrap

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const App: React.FC = () =>
+    <div className="app">
+        TODO-APP :)
+    </div>
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const makeConfig = productionMode => ({
+    devtool: "source-map",
+    entry: './src/index.tsx',
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: productionMode ? '[name].[contenthash].js' : '[name].js'
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                }
+            }
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.ejs',
+            filename: 'index.html',
+            inject: true
+        })     
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    }
+})
+
+module.exports = (env, argv) => {
+    const productionMode = argv.mode === 'production';
+    const config = makeConfig(productionMode);
+    if (productionMode) {
+        config.plugins = [new CleanWebpackPlugin(), ...config.plugins];
+    }
+    return config;
+};
